ca1b2a2ddecc9fb2d99b0c15b9289f3941f313d1
Sandrine <sandrine.yven@gmail.com>
2019-02-01 15:31:53 -0500

    Affichage des résultats proba et gains

--- src/main.c
+++ src/main.c
@@ -11,54 +11,74 @@ int main(int argc, char *argv[]) {
 	srand(time(NULL));
 
 	//Nombre d'iteration
-	size_t iterations = 10000;
-	double playerMoney = 0;
+	size_t iterations = 1000000;
+
 
 	//Test des jeux
 	if(1){
+		double playerMoney = 100;
+		double win = 0;
 		for(size_t i = 0; i < iterations; i++)
 		{
-			
-			playerMoney += playGame1();
+			double gain = playGame1();
+			playerMoney += gain;
+			if(gain >0){win++;}
 		}
-		double proba = playerMoney/iterations * 100;
-		printf(" Gain finaux:  %f \n", proba);
+		double proba = win/iterations * 100;
+		printf("[GAME1] Gains finaux:  %f \n", playerMoney);
+		printf("[GAME1] Proba:  %f \n", proba);
 	}
 	if(1){
+		double playerMoney = 100;
+		double win = 0;
 		for(size_t i = 0; i < iterations; i++)
 		{
-			
-			playerMoney += playGame2();
+			double gain = playGame2();
+			playerMoney += gain;
+			if(gain >0){win++;}
 		}
-		double proba = playerMoney/iterations * 100;
-		printf(" Gain finaux:  %f \n", proba);
+		double proba = win/iterations * 100;
+		printf("[GAME2] Gains finaux:  %f \n", playerMoney);
+		printf("[GAME2] Proba:  %f \n", proba);
 	}
 	if(1){
+		double playerMoney = 100;
+		double win = 0;
 		for(size_t i = 0; i < iterations; i++)
 		{
-			
-			playerMoney += playGame3();
+			double gain = playGame3();
+			playerMoney += gain;
+			if(gain >0){win++;}
 		}
-		double proba = playerMoney/iterations * 100;
-		printf(" Gain finaux:  %f \n", proba);
+		double proba = win/iterations * 100;
+		printf("[GAME3] Gains finaux:  %f \n", playerMoney);
+		printf("[GAME3] Proba:  %f \n", proba);
 	}
 	if(1){
+		double playerMoney = 100;
+		double win = 0;
 		for(size_t i = 0; i < iterations; i++)
 		{
-			
-			playerMoney += playGame4();
+			double gain = playGame4();
+			playerMoney += gain;
+			if(gain >0){win++;}
 		}
-		double proba = playerMoney/iterations * 100;
-		printf(" Gain finaux:  %f \n", proba);
+		double proba = win/iterations * 100;
+		printf("[GAME4] Gains finaux:  %f \n", playerMoney);
+		printf("[GAME4] Proba:  %f \n", proba);
 	}
 	if(1){
+		double playerMoney = 100;
+		double win = 0;
 		for(size_t i = 0; i < iterations; i++)
 		{
-			
-			playerMoney += playGame2();
+			double gain = playGame5();
+			playerMoney += gain;
+			if(gain >0){win++;}
 		}
-		double proba = playerMoney/iterations * 100;
-		printf(" Gain finaux:  %f \n", proba);
+		double proba = win/iterations * 100;
+		printf("[GAME5] Gains finaux:  %f \n", playerMoney);
+		printf("[GAME5] Proba:  %f \n", proba);
 	}
 
 	return 0;
